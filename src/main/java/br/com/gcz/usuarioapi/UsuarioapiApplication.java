95188b8c53bc750032bdf4ce0331a6c9c632a85c
Gustavo Zucolotto <guzuc@outlook.com>
2021-04-29 21:57:33 -0300

    iniciando repositório

---
+++ src/main/java/br/com/gcz/usuarioapi/UsuarioapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.gcz.usuarioapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuarioapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuarioapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gcz/usuarioapi/UsuarioapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gcz.usuarioapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
