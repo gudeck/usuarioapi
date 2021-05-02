70e26bfc1a7cd0dea81dad637bcaa96a1b4aa918
gudeck <guzuc@outlook.com>
2021-05-02 02:58:31 -0300

    alterando extensão do arquivo de configuração e reconfigurando feign
    Signed-off-by: gudeck <guzuc@outlook.com>

--- src/main/java/br/com/gcz/usuarioapi/consulta_cep/ViaCepClient.java
+++ src/main/java/br/com/gcz/usuarioapi/consulta_cep/ViaCepClient.java
@@ -4,10 +4,13 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "viacepclient", url = "${feign.viacep.url}")
+@FeignClient(name = "viacepclient", url = "https://viacep.com.br/ws/")
 public interface ViaCepClient {
 
+    @GetMapping("{uf}/{cidade}/{logradouro}/json")
+    ViaCepResponse buscarCep(@PathVariable String uf, @PathVariable String cidade, @PathVariable String logradouro);
+
     @GetMapping("{cep}/json")
-    Object buscarPorCep(@PathVariable String cep);
+    ViaCepResponse buscarPorCep(@PathVariable String cep);
 
 }
