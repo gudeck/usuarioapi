37313b7870d27ee6b72989c98b632a5e5b70aef9
gudeck <guzuc@outlook.com>
2021-05-02 03:08:49 -0300

    fluxo de associar endereço completo
    Signed-off-by: gudeck <guzuc@outlook.com>

---
+++ src/main/java/br/com/gcz/usuarioapi/endereco/EnderecoRequest.java
@@ -0,0 +1,45 @@
+package br.com.gcz.usuarioapi.endereco;
+
+import br.com.gcz.usuarioapi.validation.constraint.CEP;
+
+import javax.validation.constraints.NotBlank;
+
+public final class EnderecoRequest {
+
+    @CEP(message = "{endereco.invalido.cep}")
+    @NotBlank(message = "{campo.vazio}")
+    private final String cep;
+
+    @NotBlank(message = "{campo.vazio}")
+    private final String estado;
+
+    @NotBlank(message = "{campo.vazio}")
+    private final String cidade;
+
+    @NotBlank(message = "{campo.vazio}")
+    private final String bairro;
+
+    @NotBlank(message = "{campo.vazio}")
+    private final String logradouro;
+
+    @NotBlank(message = "{campo.vazio}")
+    private final String numero;
+
+    @NotBlank(message = "{campo.vazio}")
+    private final String complemento;
+
+    public EnderecoRequest(String cep, String estado, String cidade, String bairro,
+                           String logradouro, String numero, String complemento) {
+        this.cep = cep;
+        this.estado = estado;
+        this.cidade = cidade;
+        this.bairro = bairro;
+        this.logradouro = logradouro;
+        this.numero = numero;
+        this.complemento = complemento;
+    }
+
+    public Endereco toEndereco(Long idUsuario) {
+        return new Endereco(idUsuario, cep, estado, cidade, bairro, logradouro, numero, complemento);
+    }
+}
--- src/main/java/br/com/gcz/usuarioapi/usuario/UsuarioController.java
+++ src/main/java/br/com/gcz/usuarioapi/usuario/UsuarioController.java
@@ -1,10 +1,14 @@
 package br.com.gcz.usuarioapi.usuario;
 
+import br.com.gcz.usuarioapi.endereco.Endereco;
+import br.com.gcz.usuarioapi.endereco.EnderecoRepository;
+import br.com.gcz.usuarioapi.endereco.EnderecoRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,16 +23,28 @@ import java.net.URI;
 public class UsuarioController {
 
     private final UsuarioRepository usuarioRepository;
+    private final EnderecoRepository enderecoRepository;
 
-    public UsuarioController(UsuarioRepository usuarioRepository) {
+    public UsuarioController(UsuarioRepository usuarioRepository, EnderecoRepository enderecoRepository) {
         this.usuarioRepository = usuarioRepository;
+        this.enderecoRepository = enderecoRepository;
+    }
+
+    @PutMapping("/{id}/enderecos")
+    public ResponseEntity<Void> associarEndereco(@PathVariable Long id, @RequestBody @Valid EnderecoRequest request) {
+        Endereco endereco = enderecoRepository.save(request.toEndereco(id));
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}/enderecos/{id}").buildAndExpand(id, endereco.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Usuario> buscarPorId(@PathVariable Long id) {
-        Usuario usuario = usuarioRepository.findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "registro.naoEncontrado"));
-        return ResponseEntity.ok(usuario);
+    public ResponseEntity<UsuarioResponse> buscarPorId(@PathVariable Long id) {
+        UsuarioResponse response = usuarioRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "registro.naoEncontrado"))
+                .toUsuarioResponse();
+        response.setEnderecos(enderecoRepository.findAllByUsuarioId(response.getId()));
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping
